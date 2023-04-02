c9700184d60903b40eb3b689847af8fa9da4946e
keval958423 <129648978+keval958423@users.noreply.github.com>
2023-04-02 12:38:30 +0530

    Update practical_7.py

--- practical_7.py
+++ practical_7.py
@@ -3,25 +3,11 @@
 #AIM: PRACTICAL-7
 #GITHUB LINK: https://github.com/AneriP02/PIP_Practicals.git
 
-def Lapindrome(str):
-    strlen = len(str)
-    if(strlen%2==0):
-        a = str[:(strlen//2)]
-        b = str[(strlen//2):]
-    else: 
-        a = str[:(strlen//2)]
-        b = str[(strlen//2)+1:]
-    la = list(a)
-    la.sort()
-    lb = list(b)
-    lb.sort()
-    if (la==lb):
-        print("YES")
-    else:
-        print("NO")
+def is_lapindrome(s):
+    return sorted(s[:len(s)//2]) == sorted(s[(len(s)+1)//2:])
 
 t = int(input())
 
 for i in range(t):
-    text =  input()
-    Lapindrome(text)
+    text = input()
+    print("YES" if is_lapindrome(text) else "NO")
